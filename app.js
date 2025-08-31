edfc773f5ab4c6e7dc7a798034747446145220f3
unknown <2004.aakash.gupta@gmail.com>
2025-08-31 16:28:13 +0530

    djj

--- app.js
+++ app.js
@@ -1,42 +1,31 @@
+require('dotenv').config();
 const express = require('express');
 const connection = require('./utils/mysql_connection');
-const session = require("express-session")
+const session = require("express-session");
 const cookie_parser = require('cookie-parser');
-const ejs = require('ejs');
-const path = require('path')
+const path = require('path');
 const adminRoutes = require('./routes/admin_user/admin-userRoutes');
-const allUserRouteRoutes = require('./routes/all_user/allUserRoute')
+const allUserRouteRoutes = require('./routes/all_user/allUserRoute');
+
 const app = express();
 
-
-
 app.use(express.json());
-app.use(express.urlencoded({ extends: true }));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
-app.use(session({ secret: "xyz", resave: false, saveUninitialized: false }))
+app.set('views', path.join(__dirname, 'views'));
 app.use(cookie_parser());
+
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'default-secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.use(express.static('public'));
-app.use('/admin-user', adminRoutes)
-app.use('/all-user', allUserRouteRoutes)
+app.use('/admin-user', adminRoutes);
+app.use('/all-user', allUserRouteRoutes);
 
-const data={name:"aakash",age:20};
-const data1={...data};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000);
-
-console.log("server listen on port 3000");
-console.log("http://localhost:3000/");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
